c69dec938737b6cce1ebcc9e9ab2947689a7ff0f
CETYSDAM <CETYSDAM@DESKTOP-HMUGL0C>
2020-04-11 23:46:14 +0200

    Agregado la clase Conexion

---
+++ EjemploGrupo_2020/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploGrupo_2020
+{
+    class Conexion
+    {
+
+    }
+}
